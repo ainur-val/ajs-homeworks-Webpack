2a1e10d1b74ed4cf3c310b6e171f86c66ed8cdb2
Aynur Valiev <valiev.ainur.1991@gmail.com>
2025-06-25 21:40:03 +0500

    Внес исправления после проверки

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
-console.log('app worked');
+import './domain.js';
+import './game.js';
+
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+const game = new Game();
+game.start();
+
+console.log('app worked')
